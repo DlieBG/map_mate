b2b0e1b1e6fce1acc679d3c2450fe6b08fa184ca
Benedikt Schwering <mail@benedikt-schwering.de>
2024-08-13 13:37:27 +0200

    feat(api): add project view

--- api/src/models.py
+++ api/src/models.py
@@ -4,8 +4,12 @@ from typing import Annotated, Optional
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class Point(BaseModel):
-    x: float
-    y: float
+    lat: float
+    lng: float
+
+class MapView(BaseModel):
+    point: Point
+    zoom: int
 
 class Project(BaseModel):
     id: PyObjectId = Field(
@@ -13,10 +17,12 @@ class Project(BaseModel):
     )
     name: str
     description: str
+    view: MapView
     goal: Optional[Point]
 
 class ProjectDto(BaseModel):
     name: str
     description: str
+    view: MapView
     goal: Optional[Point] = None
     
