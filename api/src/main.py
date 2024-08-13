ac528e832de848ac895c45e356af05478bc6130f
Benedikt Schwering <mail@benedikt-schwering.de>
2024-08-13 10:44:44 +0200

    feat(api): add google maps api key service

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,8 @@
 from project import router as project_router
+from map import router as map_router
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(project_router)
+app.include_router(map_router)
