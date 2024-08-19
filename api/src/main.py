b6c720d4e8d8ff7c4c22c4c644945c155d079c3a
Benedikt Schwering <mail@benedikt-schwering.de>
2024-08-19 18:42:33 +0200

    feat(api): add point routes

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,10 @@
 from project import router as project_router
+from point import router as point_router
 from map import router as map_router
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(project_router)
+app.include_router(point_router)
 app.include_router(map_router)
